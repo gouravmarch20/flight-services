5f9a990253bceb872f6d6e127d0b711ffa2352ce
gourav <gaurav.mishra@kogo.ai>
2024-09-08 20:20:08 +0530

    city migration , model

---
+++ src/seeders/20240908125949-add-airplances.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Airplanes", [
+      {
+        modelNumber: "airbus320",
+        capicity: 200,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        modelNumber: "boing729",
+        capicity: 210,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
